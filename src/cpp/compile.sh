23bf9eac5ec7eea330c93a1a59b730de84c5ac58
Lim Li Xuan <limlixuan@u.nus.edu>
2026-06-19 17:04:00 +0800

    Fix libpq compilation headers in compile.sh for Linux CI

--- src/cpp/compile.sh
+++ src/cpp/compile.sh
@@ -3,9 +3,9 @@
 set -e
 
 echo "Compiling event_study_engine.cpp..."
-g++ -O3 -std=c++17 event_study_engine.cpp -o event_study_engine -lstdc++fs -lpq
+g++ -O3 -std=c++17 -I$(pg_config --includedir) -L$(pg_config --libdir) event_study_engine.cpp -o event_study_engine -lstdc++fs -lpq
 
 echo "Compiling backtester.cpp..."
-g++ -O3 -std=c++17 backtester.cpp -o backtester -lstdc++fs -lpq
+g++ -O3 -std=c++17 -I$(pg_config --includedir) -L$(pg_config --libdir) backtester.cpp -o backtester -lstdc++fs -lpq
 
 echo "Compilation successful! Executables created in src/cpp/"
